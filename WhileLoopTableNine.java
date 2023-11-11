53b119711843deeb974bc8ff603eef4149cc5fff
Rajesandhya <149239590+Rajesandhya@users.noreply.github.com>
2023-11-11 13:14:06 +0000

    Add files via upload

---
+++ WhileLoopTableNine.java
@@ -0,0 +1,15 @@
+package whileloop;
+
+public class WhileLoopTableNine {
+
+    public static void main(String[] args) {
+
+        int i=1;
+        while ( i<=10 )
+        {
+            System.out.println( "9 X " + i + " = " + i*9); i++;
+
+        }
+
+    }
+}
